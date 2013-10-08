990e64d03a84c500da6e8aac995a9a3721a902e2
wartalker <wartalker@gmail.com>
2013-10-08 14:56:08 +0800

    fork

--- mt-signal/main.c
+++ mt-signal/main.c
@@ -26,15 +26,15 @@
 static int run1 = 1;
 static int run2 = 1;
 
-void *
-th_fun(void *arg) {
-    while (*(int *)arg) {
-        fprintf(stdout, "%u: hello world!\n", pthread_self());
-        sleep(2);
-    }
+void * th_fun(void *arg)
+{
+	while (*(int *)arg) {
+		fprintf(stdout, "%u: hello world!\n", pthread_self());
+		sleep(2);
+	}
 
-    fprintf(stdout, "%u: exit\n", pthread_self());
-    pthread_exit(0);
+	fprintf(stdout, "%u: exit\n", pthread_self());
+	pthread_exit(0);
 }
 
 /*
@@ -42,34 +42,34 @@ th_fun(void *arg) {
  */
 
 #if 0
-void
-sig_handler(int sig) {
-    fprintf(stdout, "Sig handle %d\n", sig);
-    run1 = 0;
-    run2 = 0;
+void sig_handler(int sig)
+{
+	fprintf(stdout, "Sig handle %d\n", sig);
+	run1 = 0;
+	run2 = 0;
 }
 
-int
-main(int argc, char **argv) {
-    struct sigaction sig_act;
-    pthread_t t1;
-    pthread_t t2;
+int main(int argc, char **argv)
+{
+	struct sigaction sig_act;
+	pthread_t t1;
+	pthread_t t2;
 
-    memset((void *)&sig_act, 0, sizeof(sig_act));
-    sigemptyset(&sig_act.sa_mask);
-    sig_act.sa_handler = sig_handler;
-    sig_act.sa_flags = 0;
+	memset((void *)&sig_act, 0, sizeof(sig_act));
+	sigemptyset(&sig_act.sa_mask);
+	sig_act.sa_handler = sig_handler;
+	sig_act.sa_flags = 0;
 
-    sigaction(SIGINT, &sig_act, NULL);
-    sigaction(SIGTERM, &sig_act, NULL);
-    sigaction(SIGQUIT, &sig_act, NULL);
+	sigaction(SIGINT, &sig_act, NULL);
+	sigaction(SIGTERM, &sig_act, NULL);
+	sigaction(SIGQUIT, &sig_act, NULL);
 
-    pthread_create(&t1, NULL, th_fun, &run1);
-    pthread_create(&t2, NULL, th_fun, &run2);
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
+	pthread_create(&t1, NULL, th_fun, &run1);
+	pthread_create(&t2, NULL, th_fun, &run2);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 #endif
 
@@ -78,45 +78,45 @@ main(int argc, char **argv) {
  */
 
 #if 1
-void *
-th_sig(void *arg) {
-    sigset_t *set = arg;
-    int sig;
+void *th_sig(void *arg)
+{
+	sigset_t *set = arg;
+	int sig;
 
-    while (1) {
-        sigwait(set, &sig);
-        if ((SIGINT == sig) || (SIGTERM == sig) || (SIGQUIT == sig)) {
-            fprintf(stdout, "signal handle %d\n", sig);
-            run1 = 0;
-            run2 = 0;
-            break;
-        }
-    }
-    pthread_exit(0);
+	while (1) {
+		sigwait(set, &sig);
+		if ((SIGINT == sig) || (SIGTERM == sig) || (SIGQUIT == sig)) {
+			fprintf(stdout, "signal handle %d\n", sig);
+			run1 = 0;
+			run2 = 0;
+			break;
+		}
+	}
+	pthread_exit(0);
 }
 
-int
-main(int argc, char **argv) {
-    sigset_t set;
-    pthread_t ts;
-    pthread_t t1;
-    pthread_t t2;
+int main(int argc, char *argv[])
+{
+	sigset_t set;
+	pthread_t ts;
+	pthread_t t1;
+	pthread_t t2;
 
-    sigemptyset(&set);
-    sigaddset(&set, SIGINT);
-    sigaddset(&set, SIGTERM);
-    sigaddset(&set, SIGQUIT);
-    
-    pthread_sigmask(SIG_BLOCK, &set, NULL);
+	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
+	sigaddset(&set, SIGTERM);
+	sigaddset(&set, SIGQUIT);
 
-    pthread_create(&ts, NULL, th_sig, &set);
-    pthread_create(&t1, NULL, th_fun, &run1);
-    pthread_create(&t2, NULL, th_fun, &run2);
+	pthread_sigmask(SIG_BLOCK, &set, NULL);
 
-    pthread_join(ts, NULL);
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
+	pthread_create(&ts, NULL, th_sig, &set);
+	pthread_create(&t1, NULL, th_fun, &run1);
+	pthread_create(&t2, NULL, th_fun, &run2);
 
-    return EXIT_SUCCESS;
+	pthread_join(ts, NULL);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+
+	return EXIT_SUCCESS;
 }
 #endif
